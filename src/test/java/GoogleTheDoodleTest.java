8782584282467ab1039f53919d54ef10e382c739
Joerg Henning <henning.joerg@gmail.com>
2015-01-27 09:25:08 +0700

    Initial commit, basic test

---
+++ src/test/java/GoogleTheDoodleTest.java
@@ -0,0 +1,57 @@
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GoogleTheDoodleTest {
+
+    private WebDriver driver;
+    private ScreenShooter shooter;
+
+    @Before
+    public void openBrowser() {
+        String baseUrl = "http://www.google.com";
+        driver = new FirefoxDriver();
+        driver.get(baseUrl);
+        shooter = new ScreenShooter();
+    }
+
+    @After
+    public void closeBrowser() throws IOException {
+        shooter.shoot("/home/jhenning/Devel/Sandbox/selenium-demo/screenshot.png");
+        driver.quit();
+    }
+
+    @Test
+    public void searchTheDoodle() {
+        assertEquals("The page title should equal Google at the start of the test.", "Google", driver.getTitle());
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("The Doodle");
+        searchField.submit();
+        assertTrue((new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                return webDriver.getTitle().toLowerCase().startsWith("the doodle");
+            }
+        }));
+    }
+
+    private class ScreenShooter {
+
+        void shoot(String fileName) throws IOException {
+            File shot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(shot, new File(fileName));
+        }
+    }
+
+}
